b59efd9b392d131d385cdebb29fd6d88245c9bf6
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-31 12:27:09 +0530

    1 file modified

--- pivotal_ui.js
+++ pivotal_ui.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import {Icon} from 'pivotal-ui/react/iconography';
 import {DefaultButton, PrimaryButton, DangerButton, BrandButton} from 'pivotal-ui/react/buttons';
 
 class PivotalUI extends Component{
   render(){
     return(
       <React.Fragment>
-        <DefaultButton /*flat="true"*/ fullWidth="true" small icon={<Icon src="add"/>}>Click Me</DefaultButton>
+        <DefaultButton /*flat="true"*/ fullWidth="true" small>}>Click Me</DefaultButton>
         <PrimaryButton /*flat="true"*/ fullWidth="true" small>Click Me</PrimaryButton>
         <DangerButton /*flat="true"*/ fullWidth="true" small>Click Me</DangerButton>
+        <Icon src="react"/>
         <BrandButton /*flat="true"*/ fullWidth="true" small>Click Me</BrandButton>    
       </React.Fragment>
     );
