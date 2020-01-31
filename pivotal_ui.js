83a118e9d6ad3b6524fa7f74e58c12aa0b7710ff
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-31 12:05:39 +0530

    3 files modified

---
+++ pivotal_ui.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {DefaultButton} from 'pivotal-ui/react/buttons';
+
+class PivotalUI extends React.Component{
+  render(){
+    return(
+      <DefaultButton>Click Me</DefaultButton>
+    )
+  }
+}
