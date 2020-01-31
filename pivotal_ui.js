43ea3ecd5e9575e6e1bebe41bfb310f1f76c8196
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-31 14:52:14 +0530

    1 file modified

--- pivotal_ui.js
+++ pivotal_ui.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {ThemeProvider} from 'pivotal-ui/react/theme-context';
+import {Grid, FlexCol} from 'pivotal-ui/react/flex-grids';
 import {Icon} from 'pivotal-ui/react/iconography';
 import {DefaultButton, PrimaryButton, DangerButton, BrandButton} from 'pivotal-ui/react/buttons';
 
@@ -6,10 +8,14 @@ class PivotalUI extends Component{
   render(){
     return(
       <React.Fragment>
-        <DefaultButton /*flat="true"*/ fullWidth="true" small icon={<Icon src="add"/>}>Click Me</DefaultButton>
-        <PrimaryButton href="" /*flat="true"*/ fullWidth="true" small>Click Me</PrimaryButton>
-        <DangerButton /*flat="true"*/ fullWidth="true" small>Click Me</DangerButton>
-        <BrandButton /*flat="true"*/ fullWidth="true" small>Click Me</BrandButton>    
+        <div className="bg-dark-gray pal">
+          <ThemeProvider theme="dark">
+            <DefaultButton /*flat="true"*/ fullWidth="true" small icon={<Icon src="add"/>}>Click Me</DefaultButton>
+            <PrimaryButton href="" /*flat="true"*/ fullWidth="true" small>Click Me</PrimaryButton>
+            <DangerButton /*flat="true"*/ fullWidth="true" small>Click Me</DangerButton>
+            <BrandButton /*flat="true"*/ fullWidth="true" small>Click Me</BrandButton>   
+          </ThemeProvider> 
+        </div>
       </React.Fragment>
     );
   }
