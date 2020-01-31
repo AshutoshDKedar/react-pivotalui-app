effd107779e649627d3309b9ca5f10d03add31d2
AshutoshDKedar <ashutoshdkedar@gmail.com>
2020-01-31 14:57:47 +0530

    1 file modified

--- pivotal_ui.js
+++ pivotal_ui.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {ThemeProvider} from 'pivotal-ui/react/theme-context';
-import {Grid, FlexCol} from 'pivotal-ui/react/flex-grids';
+/*import {Grid, FlexCol} from 'pivotal-ui/react/flex-grids';*/
+import 'pivotal-ui/css/flex-grids'
 import {Icon} from 'pivotal-ui/react/iconography';
 import {DefaultButton, PrimaryButton, DangerButton, BrandButton} from 'pivotal-ui/react/buttons';
 
@@ -10,10 +11,22 @@ class PivotalUI extends Component{
       <React.Fragment>
         <div className="bg-dark-gray pal">
           <ThemeProvider theme="dark">
-            <DefaultButton /*flat="true"*/ fullWidth="true" small icon={<Icon src="add"/>}>Click Me</DefaultButton>
-            <PrimaryButton href="" /*flat="true"*/ fullWidth="true" small>Click Me</PrimaryButton>
-            <DangerButton /*flat="true"*/ fullWidth="true" small>Click Me</DangerButton>
-            <BrandButton /*flat="true"*/ fullWidth="true" small>Click Me</BrandButton>   
+            <div>
+              <div class="grid grid-nogutter grid-show">
+                <div class="col">
+                  <DefaultButton /*flat="true" fullWidth="true"*/ small icon={<Icon src="add"/>}>Click Me</DefaultButton>
+                </div>
+                <div class="col">
+                  <PrimaryButton href="" /*flat="true" fullWidth="true"*/ small>Click Me</PrimaryButton>
+                </div>
+                <div class="col">
+                  <DangerButton /*flat="true" fullWidth="true"*/ small>Click Me</DangerButton>
+                </div>
+                <div class="col">
+                  <BrandButton /*flat="true" fullWidth="true"*/ small>Click Me</BrandButton>   
+                </div>
+              </div>
+            </div>
           </ThemeProvider> 
         </div>
       </React.Fragment>
